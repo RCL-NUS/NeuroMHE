731022b4e71e33784de8882545c62b7419517427
Wang Bingheng <70559054+BinghengNUS@users.noreply.github.com>
2023-10-11 23:15:22 +0800

    Update Robust_Flight.py

--- SecVII-B (source code)/Robust_Flight.py
+++ SecVII-B (source code)/Robust_Flight.py
@@ -82,7 +82,7 @@ class Controller:
         # Trajectory tracking errors
         ep = p - ref_p
         ev = v - ref_v
-        # Desired force in inertial frame for the norminal dynamics
+        # Desired force in inertial frame
         Fd = -np.matmul(self.kp, ep) - np.matmul(self.kv, ev) + self.m*self.g*self.ez + self.m*ref_a -df_Imh
         # Desired total thruster force fd
         fd = np.inner(Fd.T, np.transpose(np.matmul(Rb, self.ez))) # total thrust projected into the current body z axis
