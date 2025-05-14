3d7d16b6fffea6f1a5c2a5e100bdeb7e1f419742
Wang Bingheng <70559054+BinghengNUS@users.noreply.github.com>
2025-05-14 15:56:06 +0800

    Update Robust_Flight.py

--- SecVII-B (source code)/Robust_Flight.py
+++ SecVII-B (source code)/Robust_Flight.py
@@ -91,7 +91,7 @@ class Controller:
         """
         # Construct the desired rotation matrix (from body frame to inertial frame)
         b3c = Fd/LA.norm(Fd) # b3c = -A/norm(A), so A = -Fd. Time-derivative of df_Imh is omited.
-        b2c = np.matmul(self.skew_sym(b3c), b1_d)/LA.norm(np.matmul(self.skew_sym(b3c), b1_d)) # b2c = -C/norm(C), so C = skew_sym(b1_d)@b3c
+        b2c = np.matmul(self.skew_sym(b3c), b1_d)/LA.norm(np.matmul(self.skew_sym(b3c), b1_d)) # b2c = -C/norm(C), so C = skew_sym(b1_d)@b3c, corresponding to Eq.(36) in [1].
         b1c = np.matmul(self.skew_sym(b2c), b3c)
         Rbd = np.hstack((b1c, b2c, b3c))
         R_B_dh = np.array([[Rbd[0,0], Rbd[0,1], Rbd[0,2], Rbd[1,0], Rbd[1,1], Rbd[1,2], Rbd[2,0], Rbd[2,1], Rbd[2,2]]]).T
